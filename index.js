38b4307a5fe95e1311ec549e7341f21db174b32f
salapao2136 <potook_02@hotmail.com>
2016-05-15 16:11:05 +0700

    new configg

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ var corsOptions = {
 // app.use(cors()) full 
 app.set('port', (process.env.PORT || 90))
 app.get('/', cors(corsOptions), function (req, res) {
-  res.json({name: 'hello world.'})
+  res.json({contacts:{
+  	name:'Nati' , tel :'098-325-3214',
+  	name:'Somchai' , tel :'098-334-4245',
+  	name:'Pradii' , tel :'098-543-8785',
+  	name:'nati' , tel :'098-346-2347'
+  }})
 })
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'))
