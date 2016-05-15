413e6b22e026ca7df921dd10303fdbcd76ec6d65
salapao2136 <potook_02@hotmail.com>
2016-05-15 14:11:53 +0700

    cors

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 90))
+app.get('/', function (req, res) {
+  res.json({name: 'hello world.'})
+})
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
